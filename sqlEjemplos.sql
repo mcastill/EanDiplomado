ec01d6531e3a3249be5a8ef0e7adae9f385aca92
Mario Castillo <mcastill.316@gmail.com>
2018-09-08 08:05:52 -0500

    Update sqlEjemplos.sql

--- sqlEjemplos.sql
+++ sqlEjemplos.sql
@@ -12,8 +12,8 @@ OppositionTeam VARCHAR(100),
 Player VARCHAR(100),
 Runs INT
 );
-INSERT INTO @pvt_input VALUES('Delhi Daredevils', 'Rohit Sharma', 50) -- Runs scored by Rohit Sharma against the team 'Delhi Daredevils'
-INSERT INTO @pvt_input VALUES('Delhi Daredevils', 'Rohit Sharma', 20) -- Another record for runs scored by Rohit Sharma against the team 'Delhi Daredevils', this will be aggregated when we apply pivot
+INSERT INTO @pvt_input VALUES('Delhi Daredevils', 'Rohit Sharma', 50) 
+INSERT INTO @pvt_input VALUES('Delhi Daredevils', 'Rohit Sharma', 20) 
 INSERT INTO @pvt_input VALUES('Delhi Daredevils', 'Bumrah', 40)
 INSERT INTO @pvt_input VALUES('Delhi Daredevils', 'Gopal', 70)
 INSERT INTO @pvt_input VALUES('Delhi Daredevils', 'McClenaghan', 100)
@@ -41,15 +41,14 @@ INSERT INTO @pvt_input VALUES('Pune Sunrisers', 'Harbhajan', 65)
  
 SELECT * FROM @pvt_input;
 
--- PIVOT Example Converting Column values (Players) to Column Names
+
 SELECT OppositionTeam,  [Rohit Sharma], [Bumrah], [Gopal], [McClenaghan], [Harbhajan]
 FROM
 (SELECT OppositionTeam, Player, Runs FROM @pvt_input) AS p
 PIVOT
 (
--- Here pivot expects an aggregate function like SUM, AVG, MIN, MAX
 SUM (Runs)
--- These Column Values will become Column names for Pivot result
+
 FOR Player IN ([Rohit Sharma], [Bumrah], [Gopal], [McClenaghan], [Harbhajan])
 ) AS pvt
 ORDER BY pvt.OppositionTeam;
@@ -73,7 +72,6 @@ INSERT INTO @unpivot_input VALUES ('Rajasthan Royals', 80, 50, 54, 51, 65)
  
 SELECT * FROM @unpivot_input;
 
--- Create the table and insert values as portrayed in the previous example.  
 CREATE TABLE pvt (VendorID int, Emp1 int, Emp2 int,  
     Emp3 int, Emp4 int, Emp5 int);  
 GO  
@@ -84,7 +82,6 @@ INSERT INTO pvt VALUES (4,4,2,5,5,4);
 INSERT INTO pvt VALUES (5,5,1,5,5,5);  
 GO  
 select * from pvt;
--- Unpivot the table.  
 SELECT VendorID, Employee, Orders  
 FROM   
    (SELECT VendorID, Emp1, Emp2, Emp3, Emp4, Emp5  
